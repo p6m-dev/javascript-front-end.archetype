3e4314dbf2e23cf3ebf624705a4f3b1ceb19d75a
Victor Yurkin <victor.yurkin@naxgrp.com>
2024-01-23 15:55:37 -0500

    Updated features implementation

--- layouts_base/{{ root-directory }}/libs/features/profile/vite.config.ts
+++ layouts_base/{{ root-directory }}/libs/features/profile/vite.config.ts
@@ -1,24 +1,27 @@
-/// <reference types="vitest" />
+/// <reference types='vitest' />
 import { join } from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/features-profile',
+  cacheDir: '../../../node_modules/.vite/features-profile',
 
   plugins: [
+    react(),
+    nxViteTsPaths(),
     dts({
       entryRoot: 'src',
       tsConfigFilePath: join(__dirname, 'tsconfig.lib.json'),
       skipDiagnostics: true,
     }),
-    react(),
-    viteTsConfigPaths({
-      root: '../../',
-    }),
   ],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
@@ -41,7 +44,7 @@ export default defineConfig({
   test: {
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest',
+      dir: '../../../node_modules/.vitest',
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
